75b8f99132effad7d30da43d7571fd8e3ae8825d
openvault <157576678+mazharmecci@users.noreply.github.com>
2026-01-10 17:14:11 +0530

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,7 +1,14 @@
 from fastapi import FastAPI
+from app.config import settings
 
-app = FastAPI()
+app = FastAPI(
+    title=settings.APP_NAME,
+    version=settings.API_VERSION,
+)
 
 @app.get("/")
 def root():
-    return {"status": "ISTOS AI Service running"}
+    return {
+        "status": "ISTOS AI Service running",
+        "environment": settings.ENV,
+    }
