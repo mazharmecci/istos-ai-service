986a562be90f5d3941617ef865b27d4436a1b715
openvault <157576678+mazharmecci@users.noreply.github.com>
2026-01-10 14:58:27 +0530

    Update config.py

--- app/config.py
+++ app/config.py
@@ -1,1 +1,12 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+class Settings:
+    APP_NAME = "ISTOS AI Service"
+    ENV = os.getenv("ENV", "development")
+    API_VERSION = "v1"
+
+settings = Settings()
 
