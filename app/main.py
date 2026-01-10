961610bcdeb1f19f9c102774f0b46dc79424a4f6
openvault <157576678+mazharmecci@users.noreply.github.com>
2026-01-10 15:09:57 +0530

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,4 +1,11 @@
 from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get("/")
+def root():
+    return {"status": "ISTOS AI Service running"}
+
 from app.schemas import (
     QuoteAnalysisRequest,
     QuoteAnalysisResponse
